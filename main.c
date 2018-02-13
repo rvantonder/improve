a76b543c71d6f179be8d023ec8726c68e7b85164
Rijnard van Tonder <rvantonder@gmail.com>
2018-02-13 11:05:46 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void) {
+  int c;
+  FILE *file;
+  file = fopen("test.txt", "r");
+  if (file) {
+      while ((c = getc(file)) != EOF)
+          putchar(c);
+      fclose(file);
+  }
+}
